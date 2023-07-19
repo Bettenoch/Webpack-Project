33c1e54676112a4ee5b6c632a3a314f6169b2a36
Bett Enock <bettenock968@gmail.com>
2023-07-19 11:43:17 -0700

    store and fetch items from local storage

---
+++ src/modules/LocalStorage.js
@@ -0,0 +1,15 @@
+import { newTasks } from "./DisplayTask.js";
+
+export const store = () => {
+    localStorage.setItem('tasks', JSON.stringify(newTasks.tasks));
+}
+
+export const fetchItems = () => {
+    const fetchedTodos =  JSON.parse(localStorage.getItem('tasks'));
+    if (fetchedTodos === null) {
+        return;
+    }
+    fetchedTodos.forEach ((todo) => {
+        newTasks.addItems(todo);
+    });
+};
