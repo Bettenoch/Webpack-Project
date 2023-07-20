1161bb85979332545ee9c82a419131f139adc0e4
Bett Enock <bettenock968@gmail.com>
2023-07-19 21:56:40 -0700

    Remove eslint errors

--- src/modules/LocalStorage.js
+++ src/modules/LocalStorage.js
@@ -1,15 +1,16 @@
-import { newTasks } from "./DisplayTask.js";
+/* eslint-disable linebreak-style */
+import { newTasks } from './DisplayTask.js';
 
 export const store = () => {
-    localStorage.setItem('tasks', JSON.stringify(newTasks.tasks));
-}
+  localStorage.setItem('tasks', JSON.stringify(newTasks.tasks));
+};
 
 export const fetchItems = () => {
-    const fetchedTodos =  JSON.parse(localStorage.getItem('tasks'));
-    if (fetchedTodos === null) {
-        return;
-    }
-    fetchedTodos.forEach ((todo) => {
-        newTasks.addItems(todo);
-    });
+  const fetchedTodos = JSON.parse(localStorage.getItem('tasks'));
+  if (fetchedTodos === null) {
+    return;
+  }
+  fetchedTodos.forEach((todo) => {
+    newTasks.addItems(todo);
+  });
 };
