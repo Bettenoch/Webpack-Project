d029bff2d97780347d17ac81f7d814123a163122
Bett Enock <bettenock968@gmail.com>
2023-07-19 21:56:29 -0700

    Remove eslint errors

--- src/modules/AddTask.js
+++ src/modules/AddTask.js
@@ -1,14 +1,15 @@
-import Tasks, { userInput} from "./TaskList.js";
-import { newTasks } from "./DisplayTask.js";
+/* eslint-disable linebreak-style */
+import Tasks, { userInput } from './TaskList.js';
+import { newTasks } from './DisplayTask.js';
 
 const addItems = (e) => {
-    const todo = new Tasks(userInput.value, false, newTasks.tasks.length + 1);
-    if (userInput.value === '') {
-        e.preventDefault();
-    } else {
-        userInput.value = '';
-    }
-    return todo;
+  const todo = new Tasks(userInput.value, false, newTasks.tasks.length + 1);
+  if (userInput.value === '') {
+    e.preventDefault();
+  } else {
+    userInput.value = '';
+  }
+  return todo;
 };
 
 export default addItems;
