b94e16517a009a2faaf8ed1ed05a582e626f2942
Bett Enock <bettenock968@gmail.com>
2023-07-29 20:04:50 -0700

    Add test file

---
+++ src/TestDocs/TestFile.test.js
@@ -0,0 +1,47 @@
+import 'jest-localstorage-mock';
+import fetchTodos from '../modules/FetchFromLs.js';
+import addItems from '../modules/DisplayTask.js';
+import removeTodo from '../modules/DeleteTask.js';
+import { currVal, editTask } from '../modules/editTask.js';
+// import isChecked from '../modules/CheckTodo.js';
+import clearChecked from '../modules/DeleteCompleted.js';
+import checkboxTest from './checkData.js';
+
+describe('test my functions', () => {
+  test('should add a todo to the list', () => {
+    addItems('Watch Lakers Game');
+    const data = fetchTodos();
+    expect(data.length).toBe(1);
+  });
+
+  test('Should change the todo', () => {
+    currVal('Watch Detroit Game');
+    editTask('Watch Lakers Game');
+    const data = fetchTodos();
+    expect(data[0].description).toBe('Watch Lakers Game');
+  });
+
+  test('should ensure that todo checkbox is checked', () => {
+    const description = 'Todo description';
+    const altStat = true;
+
+    const task = { description, completed: false };
+    localStorage.setItem('tasks', JSON.stringify([task]));
+
+    checkboxTest(description, altStat);
+    const data = JSON.parse(localStorage.getItem('tasks'));
+    expect(data[0].completed).toBe(altStat);
+  });
+
+  test('should remove checked todo', () => {
+    clearChecked();
+    const data = fetchTodos();
+    expect(data.length).toBe(0);
+  });
+
+  test('should remove an item in the list', () => {
+    removeTodo();
+    const data = fetchTodos();
+    expect(data.length).toBe(0);
+  });
+});
